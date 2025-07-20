6fdc5da1f56a10af303fcb2278481fd3a93acc62
Samuel Hobl <samuel.hobl@gmail.com>
2025-07-20 18:50:36 +0200

    chore: hide Navbar

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,12 +1,10 @@
 import { type FunctionComponent } from 'react';
 import { Layout } from './Layout';
-import { Navbar } from "./Navbar";
 
 type Props = { content: string };
 
 export const Page: FunctionComponent<Props> = (props) => (
   <Layout>
-    <Navbar />
     <main className="mx-auto container px-8 mt-10">
       <article className="prose max-w-none" dangerouslySetInnerHTML={{ __html: props.content }} />
     </main>
